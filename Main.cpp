5e59778d72e0265df4df24477b13d80c782bdcf8
krentin018 <krentin.naidoo18@gmail.com>
2016-03-04 22:25:52 +0200

    included main class + Bug fixes

--- Main.cpp
+++ Main.cpp
@@ -4,4 +4,20 @@ using namespace std;
 
 int main()
 {
+	int arr[10];
+	for (int k = 0; k < 10; k++)
+	{
+		arr[k] = k;
+	}
+
+	Dice obj(5, 2, arr); 
+
+	double Davg, arravg;
+
+	Davg = obj.average(obj, 8); 
+	cout << "The average of the dice is " << Davg << endl;
+
+	arravg = obj.average(arr, 9);
+	cout << "The average of the array is " << arravg << endl;
+
 }
